fcabb0d1c088201419b8006deef6d76e15ac583d
BenM4k <103831502+BenM4k@users.noreply.github.com>
2023-04-19 11:55:09 +0200

    update: featured speakers
    made the section dynamic using js

--- js/main.js
+++ js/main.js
@@ -21,3 +21,45 @@ function toggleMenu() {
 }
 
 menuBtn.addEventListener('click', toggleMenu);
+
+// feature speakers
+
+const speakers = [
+  {
+    name: 'Yochai Benkler',
+    title: 'Lorem ipsum dolor sit amet.',
+    paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, nisi esse.',
+    img: 'assets/img1.jpg',
+  },
+  {
+    name: 'Yochai Benkler',
+    title: 'Lorem ipsum dolor sit amet.',
+    paragraph: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt, nisi esse.',
+    img: 'assets/img2.jpg',
+  },
+];
+
+const featureSpeaker = document.querySelector('.featured-speakers__container');
+
+function createSpeaker(speaker) {
+  const section = `
+    <li class="featured-speakers__list">
+        <div class="list-img">
+            <img src="${speaker.img}" alt="" class="featured-speakers__img">
+        </div>
+        <div class="list-desc">
+            <h2>${speaker.name}</h2>
+            <h3><em>
+                ${speaker.title}
+            </em></h3>
+            <span class="list-desc__line"></span>
+            <p>${speaker.paragraph}</p>
+        </div>
+    </li>`;
+  return section;
+}
+
+for (let i = 0; i < speakers.length; i += 1) {
+  const speaker = createSpeaker(speakers[i]);
+  featureSpeaker.innerHTML += speaker;
+}
