9097d85b33c6ba5e0fb5f45e2b9111a8b9588a2e
BenM4k <103831502+BenM4k@users.noreply.github.com>
2023-04-19 09:49:02 +0200

    fix: linter errors
    fixed error for both eslint and stylelint

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-//navbar 
+// navbar
 const menuBtn = document.querySelector('.menu-btn');
 const burger = document.querySelector('.burger');
 const header = document.querySelector('header');
 const menuNav = document.querySelector('.menu-navbar');
 let displayMenu = false;
 
-menuBtn.addEventListener('click', toggleMenu)
-
-function toggleMenu(){
-    if(!displayMenu) {
-        burger.classList.add('open');
-        header.classList.add('open');
-        header.classList.add('open');
-        menuNav.classList.add('open');
-        displayMenu = true;
-    }else{
-        burger.classList.remove('open');
-        header.classList.remove('open');
-        menuNav.classList.remove('open');
-        displayMenu = false;
-    }
+function toggleMenu() {
+  if (!displayMenu) {
+    burger.classList.add('open');
+    header.classList.add('open');
+    header.classList.add('open');
+    menuNav.classList.add('open');
+    displayMenu = true;
+  } else {
+    burger.classList.remove('open');
+    header.classList.remove('open');
+    menuNav.classList.remove('open');
+    displayMenu = false;
+  }
 }
+
+menuBtn.addEventListener('click', toggleMenu);
